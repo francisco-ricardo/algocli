2607343d6b27cecdd608beca46eabec37c3b628a
francisco <franciscoricardo.dev@gmail.com>
2023-03-13 04:05:27 +0000

    Continue com Two Number Sum
     Changes to be committed:
    	modified:   src/two_numbers_sum/two_numbers_sum.py

--- src/two_numbers_sum/two_numbers_sum.py
+++ src/two_numbers_sum/two_numbers_sum.py
@@ -1,6 +1,20 @@
+'''
+Two Number Sum
+
+Write a function that takes in a non-empyt array fo distinct integers
+and an integer representing a target sum. If any two numbers in the
+input array sum up to the target sum, the function should return them
+in an array, in any order. If no two number sum up to the target sum,
+the function should return an empty array.
+
+Note that the target sum has to be obtained by summing two different
+integers in the array; you can not add a single integer to itself in
+order to obtain the target sum.
+
+'''
 
 
-def two_numbers_sum(array, targetSum):
+def twoNumberSum(array, targetSum):
 
     def tuples_generator(array):
         pair_heads_index = [x for x in range(0, (len(array) - 1))]
@@ -13,9 +27,17 @@ def two_numbers_sum(array, targetSum):
                 yield(array[i], array[j],)
 
 
-    def pair_sum():
-        pass            
+    def pair_sum(pairs_array, target_sum):
+        for pair in pairs_array:
+            if (pair[0] + pair[1] == target_sum):
+                return [pair[0], pair[1]]
+        else:
+            return []
 
+        
 
-    array_pairs = [x for x in tuples_generator(array)]
-    print(array_pairs)
+    pairs_array = [x for x in tuples_generator(array)]
+    print(pairs_array)
+
+    result = pair_sum(pairs_array, targetSum)
+    print(result)
