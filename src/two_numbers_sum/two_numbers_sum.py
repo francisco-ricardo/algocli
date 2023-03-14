57c62345d1d5b22a57e26676b3078668ed9fe9a0
francisco <franciscoricardo.dev@gmail.com>
2023-03-14 01:06:23 -0300

    Ajuste padroes no two numbers sum
     Changes to be committed:
    	modified:   src/two_numbers_sum/two_numbers_sum.py

--- src/two_numbers_sum/two_numbers_sum.py
+++ src/two_numbers_sum/two_numbers_sum.py
@@ -1,7 +1,7 @@
 '''
 Two Number Sum
 
-Write a function that takes in a non-empyt array fo distinct integers
+Write a function that takes in a non-empyt array of distinct integers
 and an integer representing a target sum. If any two numbers in the
 input array sum up to the target sum, the function should return them
 in an array, in any order. If no two number sum up to the target sum,
@@ -15,11 +15,30 @@ order to obtain the target sum.
 
 
 def twoNumberSum(array, targetSum):
+    '''
+    The function receives an array and an integer as parameters.
+    If any two numbers in the input array sum up to the target sum, 
+    the function should return them in an array, in any order. 
+    If no two number sum up to the target sum, the function should 
+    return an empty array.
 
-    def tuples_generator(array):
+    Parameters
+    -----------
+    1. array: input array (integers)
+    2. targetSum: the target value (integer) for testing the input array
+
+    Notes
+    -----
+    - The camel case is not commom for naming funcion in Python.
+      But it seems like it is a standard on the Algo Expert.
+      Then I decided to follow the Algo Exert standard and 
+      naming the functions using camel case.
+
+    '''
+
+    def _tuplesGenerator(array):
         pair_heads_index = [x for x in range(0, (len(array) - 1))]
         pair_tails_index = [x for x in range(1, len(array))]
-
         for i in pair_heads_index:
             for j in pair_tails_index:
                 if (j <= i):
@@ -27,7 +46,7 @@ def twoNumberSum(array, targetSum):
                 yield(array[i], array[j],)
 
 
-    def pair_sum(pairs_array, target_sum):
+    def _pairSum(pairs_array, target_sum):
         for pair in pairs_array:
             if (pair[0] + pair[1] == target_sum):
                 return [pair[0], pair[1]]
@@ -36,8 +55,8 @@ def twoNumberSum(array, targetSum):
 
         
 
-    pairs_array = [x for x in tuples_generator(array)]
+    pairs_array = [x for x in _tuplesGenerator(array)]
     print(pairs_array)
 
-    result = pair_sum(pairs_array, targetSum)
+    result = _pairSum(pairs_array, targetSum)
     print(result)
