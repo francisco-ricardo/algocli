cc5aca0536f637253d3f6edafa97108be9776db6
francisco <franciscoricardo.dev@gmail.com>
2023-03-25 13:54:50 +0000

    Rewrite two_numbers_sum with a better approach
     Changes to be committed:
    	new file:   scratch.txt
    	modified:   src/two_numbers_sum/two_numbers_sum.py

--- src/two_numbers_sum/two_numbers_sum.py
+++ src/two_numbers_sum/two_numbers_sum.py
@@ -13,7 +13,6 @@ order to obtain the target sum.
 
 '''
 
-
 def twoNumberSum(array, targetSum):
     '''
     The function receives an array and an integer as parameters.
@@ -22,6 +21,14 @@ def twoNumberSum(array, targetSum):
     If no two number sum up to the target sum, the function should 
     return an empty array.
 
+    Approach
+    ---------
+    targetSum = currentNum + potentialNum
+    potentialNum = targetSum - currentNum
+
+    Time: O(n)
+    Space: O(n)
+
     Parameters
     -----------
     1. array: input array (integers)
@@ -29,54 +36,20 @@ def twoNumberSum(array, targetSum):
 
     Notes
     -----
-    - The camel case is not common for naming functions in Python. 
+    - The camel case is not common for naming functions and variables 
+      in Python. 
       But it seems like it is a standard for Algo Expert. 
       So I decided to follow the Algo Exert standard and name the 
-      functions using camel case.
-    - Algo Expert asks for only one function for the solution. 
-      They even indicated the name of the function. So, I decided to 
-      create only one function and create helper functions inside it.
+      variables using camel case.
 
     '''
-    # Helper function
-    def _pairsGenerator(array):
-        '''
-        Generator function that receives an array as input 
-        and yield tuples of pairs of elements.
-        
-        Example:
-        [1, 2, 3] -> ((1, 2), (1, 3), (2, 3))
-        '''
-        pair_heads_index = [x for x in range(0, (len(array) - 1))]
-        pair_tails_index = [x for x in range(1, len(array))]
-        for i in pair_heads_index:
-            for j in pair_tails_index:
-                if (j <= i):
-                    continue
-                yield(array[i], array[j],)
-
-
-    # Helper function
-    def _pairSum(pairsArray, targetSum):
-        '''
-        Helper function that receives two arguments:
-        1. Array of tuples (integers)
-        2. Target sum (integer)
-        
-        Each tuple has two integers. The function loops over the array.
-        If a tuple sum up to the target sum, the function returns the 
-        two elements as an array.
-        If no tuple sum up to the target sum, the function returns an 
-        empty array.        
-        '''
-        for pair in pairsArray:
-            if (pair[0] + pair[1] == targetSum):
-                return [pair[0], pair[1]]
+    potentialNums = set()
+    for i in array:
+        potentialNum = targetSum - i
+        if (potentialNum in potentialNums):
+            return [i, potentialNum]
         else:
-            return []
+            potentialNums.add(i)
+    
+    return []
 
-        
-    # Main
-    pairsArray = [x for x in _pairsGenerator(array)]
-    result = _pairSum(pairsArray, targetSum)
-    return result
