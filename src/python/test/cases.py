8340c5e6632502019a9c00d534f9f2404f7d8312
Francisco Ricardo <franciscoricardo.dev@gmail.com>
2025-02-08 14:14:45 -0300

    Improve the cli script and the documentation
     Changes to be committed:
    	modified:   src/bin/algocli
    	modified:   src/python/test/cases.py
    	deleted:    src/python/tournament_winner/__init__.py
    	deleted:    src/python/tournament_winner/solution.py

--- src/python/test/cases.py
+++ src/python/test/cases.py
@@ -6,6 +6,7 @@ from algorithms.sorted_squared_array import run as sorted_squared_array
 from algorithms.two_numbers_sum import run as two_numbers_sum
 from algorithms.tournament_winner import run as tournament_winner
 
+
 class TestIsValidSubsequence(unittest.TestCase):
     def test_example_1(self):
         self.assertTrue(is_valid_subsequence([1, 2, 3, 4], [2, 4]))
@@ -24,22 +25,18 @@ class TestNonConstructibleChange(unittest.TestCase):
         coins = [5, 7, 1, 1, 2, 3, 22]
         expected_output = 20
         self.assertEqual(non_constructible_change(coins), expected_output)
-
     def test_example_2(self):
         coins = [1, 2, 5]
         expected_output = 4
         self.assertEqual(non_constructible_change(coins), expected_output)
-
     def test_example_3(self):
         coins = []
         expected_output = 1
         self.assertEqual(non_constructible_change(coins), expected_output)
-
     def test_example_4(self):
         coins = [1, 1, 1, 1, 1]
         expected_output = 6
         self.assertEqual(non_constructible_change(coins), expected_output)
-
     def test_example_5(self):
         coins = [1, 1, 3, 4]
         expected_output = 10
@@ -47,27 +44,22 @@ class TestNonConstructibleChange(unittest.TestCase):
 
 
 class TestSortedSquaredArray(unittest.TestCase):
-
     def test_example_1(self):
         array = [-10, 1, 2, 3, 5, 6, 8, 9]
         expected_output = [1, 4, 9, 25, 36, 64, 81, 100]
         self.assertEqual(sorted_squared_array(array), expected_output)
-
     def test_example_2(self):
         array = [-7, -3, 2, 3, 11]
         expected_output = [4, 9, 9, 49, 121]
         self.assertEqual(sorted_squared_array(array), expected_output)
-
     def test_example_3(self):
         array = [0, 2, 3, 10]
         expected_output = [0, 4, 9, 100]        
         self.assertEqual(sorted_squared_array(array), expected_output)
-
     def test_example_4(self):
         array = [-5, -3, -2, -1]
         expected_output = [1, 4, 9, 25]
         self.assertEqual(sorted_squared_array(array), expected_output)
-
     def test_example_5(self):
         array = [-4, -1, 0, 3, 10]
         expected_output = [0, 1, 9, 16, 100]
@@ -75,65 +67,56 @@ class TestSortedSquaredArray(unittest.TestCase):
 
 
 class TestTwoNumbersSum(unittest.TestCase):
-
     def test_example_1(self):
         array = [2, 7, 11, 15]
         target = 9
         expected_output = [2, 7]    
         self.assertCountEqual(two_numbers_sum(array, target), expected_output)
-
     def test_example_2(self):
         array = [3, 2, 4]
         target = 6
         expected_output = [2, 4]
         self.assertCountEqual(two_numbers_sum(array, target), expected_output)
-
     def test_example_3(self):
         array = [3, 3]
         target = 6
         expected_output = [3, 3]
         self.assertCountEqual(two_numbers_sum(array, target), expected_output)
-
     def test_example_4(self):
         array = [1, 2, 3, 4, 5]
         target = 8
         expected_output = [3, 5]
         self.assertCountEqual(two_numbers_sum(array, target), expected_output)
-
     def test_example_5(self):
         array = [0, -1, 2, -3, 1]
         target = -2
         expected_output = [1, -3]
         self.assertCountEqual(two_numbers_sum(array, target), expected_output)
 
-class TestTournamentWinner(unittest.TestCase):
 
+class TestTournamentWinner(unittest.TestCase):
     def test_example_1(self):
         competitions = [['HTML', 'C#'], ['C#', 'Python'], ['Python', 'HTML']]
         results = [0, 0, 1]
         expected_output = 'Python'
         self.assertEqual(tournament_winner(competitions, results), expected_output)
-
     def test_example_2(self):
-        competitions = [['Python', 'Perl'], ['Perl', 'C'], ['C', 'Python']]
-        results = [1, 1, 1]
-        expected_output = 'Python'
-        self.assertEqual(tournament_winner(competitions, results), expected_output)
-
-    def test_example_3(self):
-        competitions = [['Rust', 'C'], ['C', 'Java'], ['Java', 'Rust']]
-        results = [0, 0, 0]
-        expected_output = 'Java'
-        self.assertEqual(tournament_winner(competitions, results), expected_output)
-
-    def test_example_4(self):
-        competitions = [['Perl', 'Bash'], ['Perl', 'Python'], ['Bash', 'Python']]
+        competitions = [['Python', 'Perl'], ['Perl', 'C'], ['Perl', 'Python']]
         results = [1, 1, 1]
         expected_output = 'Perl'
         self.assertEqual(tournament_winner(competitions, results), expected_output)
-        
+    def test_example_3(self):
+        competitions = [['Rust', 'C'], ['C', 'Java'], ['Java', 'Rust']]
+        results = [0, 0, 1]
+        expected_output = 'Java'
+        self.assertEqual(tournament_winner(competitions, results), expected_output)
+    def test_example_4(self):
+        competitions = [['C#', 'PHP'], ['Java', 'C#'], ['PHP', 'Java']]
+        results = [1, 1, 0]
+        expected_output = 'Java'
+        self.assertEqual(tournament_winner(competitions, results), expected_output)
     def test_example_5(self):
         competitions = [['Python', 'PHP'], ['Python', 'Javascript'], ['PHP', 'Javascript']]
-        results = [0, 1, 0]
+        results = [0, 1, 1]
         expected_output = 'PHP'
         self.assertEqual(tournament_winner(competitions, results), expected_output)
